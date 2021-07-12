d7628e5a8d07b01d190fa163f9d214e588a02919
gestirn717 <86178320+gestirn717@users.noreply.github.com>
2021-07-12 23:01:57 +0900

    Add files via upload

---
+++ Excel/3_cell.py
@@ -0,0 +1,41 @@
+from openpyxl import Workbook
+from random import *
+
+wb = Workbook()
+ws = wb.active
+ws.title = "mergisheet"
+
+#A1 셀에 1 이라는 값을 입력
+ws["A1"] = 1
+ws["A2"] = 2
+ws["A3"] = 3
+
+ws["B1"] = 4
+ws["B2"] = 5
+ws["B3"] = 6
+
+print(ws["A1"]) # A1 셀의 정보를 출력
+print(ws["A1"].value) #A1 셀의  "값" 을 출력
+print(ws["A10"].value) # 값이 없을 경우 "none" 출력
+
+# row = 1,2,3
+# column = A(1), B(2), C(3), ...
+print(ws.cell(row=1, column=1).value) #ws["A1"].value
+print(ws.cell(row=1, column=2).value) #ws["B1"].value
+
+ws.cell(column=3, row=1, value=10) # ws["C1"].value = 10 / ws["C1"] = 10 이랑 동일
+print(ws["C1"].value) #c1의 값을 출력
+
+ws.cell(row=1,column=1)
+
+# 반복뮨을 이용해서 랜덤 숫자 채움
+index = 1
+for x in range(1,11):  #10개 row
+    for y in range(1,11): #10개 column
+        # ws.cell(row=x, column = y,value=randint(0,100))  # 0~100 사이의 숫자
+        ws.cell(row=x,column=y,value=index)    # 1~ 순서대로
+        index += 1
+
+
+
+wb.save("sample.xlsx")
